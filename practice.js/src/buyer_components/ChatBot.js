50a7265f804121b219288a601b752fab4ce226ef
AbhinavRy <royabhinav238@gmail.com>
2021-02-22 15:06:16 +0530

    boii chatbot

--- practice.js/src/buyer_components/ChatBot.js
+++ practice.js/src/buyer_components/ChatBot.js
@@ -4,12 +4,12 @@ export default function ChatBox(){
     const steps = [
         {
           id: '0',
-          message: 'Welcome to react chatbot!',
+          message: 'Welcome to its your boii chatbot!',
           trigger: '1',
         },
         {
           id: '1',
-          message: 'Bye!',
+          message: '\'Sup my man',
           end: true,
         },
       ];
